aef42e921780a3dd1bd0d972d6ed21908238fc4e
bc-nogueira <bcnogueira@id.uff.br>
2019-06-08 02:34:32 -0300

    Initial commit.

---
+++ App.js
@@ -0,0 +1,83 @@
+import React, {Component} from 'react';
+import { Button, StyleSheet, Text, View, ToastAndroid, PermissionsAndroid } from 'react-native';
+import * as RNFS from 'react-native-fs';
+
+export default class App extends Component {
+  state = {
+    showDownloadBtn: true,
+    showText: false
+  }
+
+  requestPermission() {
+    try {
+      const granted = PermissionsAndroid.request(
+        PermissionsAndroid.PERMISSIONS.READ_EXTERNAL_STORAGE,
+        PermissionsAndroid.PERMISSIONS.WRITE_EXTERNAL_STORAGE
+      );
+      if (granted === PermissionsAndroid.RESULTS.GRANTED) {
+        console.log('You can read external storage');
+      } else {
+        console.log('Permission denied');
+      }
+    } catch (err) {
+      console.warn(err);
+    }
+  }
+
+  makeDownload = async () => {
+    this.requestPermission();
+    downloadDir = RNFS.ExternalStorageDirectoryPath + '/Download/';
+    url = 'https://devdactic.com/html/5-simple-hacks-LBT.pdf';
+
+    downloadOptions = {
+      fromUrl: url,
+      toFile: downloadDir + 'downloadReactNative.pdf'
+    }
+
+    RNFS.downloadFile(downloadOptions).promise.then(res => {
+      this.setState({
+        showDownloadBtn: false,
+        showText: true
+      });
+    }).catch(err => {
+      ToastAndroid.show(err.message + ' - ' + err.code, ToastAndroid.SHORT);
+    });
+  }
+
+  render() {
+    return (
+      <View style={styles.container}>
+        {this.state.showDownloadBtn &&
+          <Button
+          onPress={this.makeDownload}
+            title="Make Download"
+            color="#841584"
+          />
+        }
+
+        {this.state.showText &&
+          <Text>File downloaded successfully</Text>
+        }
+      </View>
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: '#F5FCFF',
+  },
+  welcome: {
+    fontSize: 20,
+    textAlign: 'center',
+    margin: 10,
+  },
+  instructions: {
+    textAlign: 'center',
+    color: '#333333',
+    marginBottom: 5,
+  },
+});
